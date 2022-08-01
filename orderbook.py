0bb087b525d16a1195ad11707497073246edc786
sobimanna <sobimanna@gmail.com>
2022-08-01 15:24:47 +0200

    Added modify functionality

--- orderbook.py
+++ orderbook.py
@@ -13,50 +13,77 @@ class OrderBook(object):
     # for checking if the queue is empty
     def isEmpty(self):
         return len(self.queue) == 0
+
+    def makeDataForInput(self, orderNumber, data1, data2):
+        dataForInput = []
+        if (orderNumber == -1):
+            orderNumber = self.OrderNumber
+            self.OrderNumber += 1
+        dataForInput.append(orderNumber)
+        dataForInput.append(data1)
+        dataForInput.append(data2)
+        return (dataForInput)
+
+    def findItemInList(self, orderNumber):
+        itemDetails = []
+        for index in range(len(self.buy)):
+            if (int(self.buy[index][0]) == int(orderNumber)):
+                itemDetails.append("buy")
+                itemDetails.append(index)
+        if (itemDetails == []):
+            for index in range(len(self.sell)):
+                if (int(self.sell[index][0]) == int(orderNumber)):
+                    itemDetails.append("sell")
+                    itemDetails.append(index)
+        return itemDetails
+
  
     # for inserting an element in the queue
     def insert(self, data, buyOrSell):
         print(data, buyOrSell)
         dataForInput = []
         if (buyOrSell == "B"):
-            dataForInput.append(self.OrderNumber)
-            self.OrderNumber += 1
-            dataForInput.append(data[2])
-            dataForInput.append(data[3])
+            dataForInput = self.makeDataForInput(-1, data[2], data[3])
             print(dataForInput)
             self.buy.append(dataForInput)
             print(self.buy)
         elif (buyOrSell == "S"):
-            dataForInput.append(self.OrderNumber)
-            self.OrderNumber += 1
-            dataForInput.append(data[2])
-            dataForInput.append(data[3])
+            dataForInput = self.makeDataForInput(-1, data[2], data[3])
             print(dataForInput)
             self.sell.append(dataForInput)
             print(self.sell)
+
+    # for modifying queue
+    def modify(self, orderNumber, newData):
+        item = self.findItemInList(orderNumber)
+        if (item == []):
+            print("item to modify not found")
+            return
+        index = item[1]
+        print("here to modify", newData, index)
+        orderNumber = int(orderNumber)
+        myQueue.delete(orderNumber)        
+        list_ = getattr(self, item[0])
+        list_.append(newData)
+        print (list_)
+
  
     # for popping an element based on Priority
     def delete(self, orderNumber):
         orderNumber = int (orderNumber)
         index = -1
         item = None
+        orderDetails = OrderBook.findItemInList(self, orderNumber)
+        print("orderdetails", orderDetails)
         try:
-            for i in range(len(self.buy)):
-                if (self.buy[i][0] == orderNumber):
-                    index = i
-                    item = self.buy[index]
-                    print("isbuy")
-                    del self.buy[index]
-                    return item
-            if (not item):
-                for i in range(len(self.sell)):
-                    if (self.sell[i][0] == orderNumber):
-                        index = i
-                item = self.sell[index]
-                if (not item):
-                    raise IndexError
-                print ("issell")
-                del self.sell[index]
+            if (orderDetails == []):
+                raise IndexError
+            else:
+                index = orderDetails[1]
+                buyOrSell = orderDetails[0]
+                list_ = getattr(self, buyOrSell)
+                item = list_[index]
+                del list_[index]
                 return item
         except IndexError:
             print("Item is not available for delete")
@@ -70,8 +97,9 @@ if __name__ == '__main__':
         if (re.search("\ +[B|S|b|s]\ +[1-9]\d*\ +[1-9]\d*\ *$", user_input)):
             myQueue.insert(user_input.split(), user_input.split()[1].upper())
         # check if it is a valid request to modify
-        elif (re.search("[M|m]\ +[1-9]\d*\ +[1-9]\d*\ +[1-9]\d*\ *$", user_input)):
-            print("it is a request to modify")
+        elif (re.search("[M|m]\ +\d*\ +[1-9]\d*\ +[1-9]\d*\ *$", user_input)):
+            elements = user_input.split()
+            myQueue.modify(elements[1], myQueue.makeDataForInput(elements[1], elements[2], elements[3]))
         # check if it is a valid delete request
         elif (re.search("[D|d]\ +\d*\ *$", user_input)):
             print(myQueue.delete(user_input.split()[1]))
