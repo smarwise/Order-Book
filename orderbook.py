e54e3e6e9e11e91e0c957a5e86f71ade21c2b37c
sobimanna <sobimanna@gmail.com>
2022-08-01 11:37:36 +0200

    added insert into priority queue

--- orderbook.py
+++ orderbook.py
@@ -5,6 +5,8 @@ class OrderBook(object):
     def __init__(self):
         self.buy = []
         self.sell = []
+        self.sellOrderNumber = 0
+        self.buyOrderNumber = 0
  
     def __str__(self):
         return ' '.join([str(i) for i in self.queue])
@@ -14,8 +16,25 @@ class OrderBook(object):
         return len(self.queue) == 0
  
     # for inserting an element in the queue
-    def insert(self, data):
-        self.queue.append(data)
+    def insert(self, data, buyOrSell):
+        print(data, buyOrSell)
+        dataForInput = []
+        if (buyOrSell == "B"):
+            dataForInput.append(self.buyOrderNumber)
+            self.buyOrderNumber += 1
+            dataForInput.append(data[2])
+            dataForInput.append(data[3])
+            print(dataForInput)
+            self.buy.append(dataForInput)
+            print(self.buy)
+        elif (buyOrSell == "S"):
+            dataForInput.append(self.sellOrderNumber)
+            self.sellOrderNumber += 1
+            dataForInput.append(data[2])
+            dataForInput.append(data[3])
+            print(dataForInput)
+            self.sell.append(dataForInput)
+            print(self.sell)
  
     # for popping an element based on Priority
     def delete(self):
@@ -30,10 +49,14 @@ if __name__ == '__main__':
     while True:
         user_input = input("Please enter request: ")
         if (re.search("\ +[B|S|b|s]\ +[1-9]\d*\ +[1-9]\d*\ *$", user_input)):
+            myQueue.insert(user_input.split(), user_input.split()[1].upper())
             print("is a new order")
         elif (re.search("[M|m]\ +[1-9]\d*\ +[1-9]\d*\ +[1-9]\d*\ *$", user_input)):
             print("it is a request to modify")
         elif (re.search("[D|d]\ +[1-9]\d*\ *$", user_input)):
             print("it is a request to delete")
+        elif (user_input == "quit"):
+            print ("Goodbye!")
+            exit (0)
         else:
             print("Invalid Input")
