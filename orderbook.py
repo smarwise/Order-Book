82fc70ceefbf5b4b32c1b0e0726289088fa06673
sobimanna <sobimanna@gmail.com>
2022-08-02 08:49:36 +0200

    fixed ordering bugs

--- orderbook.py
+++ orderbook.py
@@ -12,59 +12,88 @@ class OrderBook(object):
  
     # for checking if the queue is empty
     def printOrderBook(self):
-        print ("buy", self.buy)
-        print ("sell", self.sell)
+        print ("buy :", self.buy)
+        print ("sell:", self.sell)
 
+    #find a sell match for a submitted buy
     def findMatchForBuy(self, data):
         newData = []
-        size = int(data[2])
-        price = int(data[3])
+        if (len(data) == 4):
+            size = int(data[2])
+            price = int(data[3])
+        else:
+            size = int(data[1])
+            price = int(data[2])
+        deleted = 0
+        #iterate priority queue and identify for any matches 
         if (len(self.sell) > 0):
             for index in range(len(self.sell)):
-                if (int(self.sell[index][2]) <= price):
-                    if (int(self.sell[index][1]) > size):
-                        self.sell[index][1] = int(self.sell[index][1]) - size
-                        size = size - int(self.sell[index][1])
-                        print("Matched with sell order number", self.sell[index][0])
-                    elif (self.sell[index][1] == size):
-                        size = size - int(self.sell[index][1])
-                        print("Matched with sell order number", self.sell[index][0])
-                        self.delete(self.sell[index][0])
+                print("index", index - deleted)
+                newIndex = index - deleted
+                if (int(self.sell[newIndex][2]) <= price):
+                    if (int(self.sell[newIndex][1]) > size):
+                        print("less", size)
+                        self.sell[newIndex][1] = int(self.sell[newIndex][1]) - size
+                        size = 0
+                        print("Matched with sell order number", self.sell[newIndex][0])
+                    elif (self.sell[newIndex][1] == size):
+                        print("equal", size)
+                        size = size - int(self.sell[newIndex][1])
+                        print("Matched with sell order number", self.sell[newIndex][0])
+                        self.delete(self.sell[newIndex][0])
+                        deleted += 1
                     else:
-                       size = size - int(self.sell[index][1])
-                       print("Matched with sell order number", self.sell[index][0])
-                       self.delete(self.sell[index][0])
+                       print("more", size)
+                       size = size - int(self.sell[newIndex][1])
+                       print("Matched with sell order number", self.sell[newIndex][0])
+                       self.delete(self.sell[newIndex][0])
+                       deleted += 1
+                #if all of a new order has been sold
                 if (size == 0):
                     newData.append(1)
                     newData.append(size)
                     return (newData)
+         #if some of a new order has been sold
         newData.append(0)
         newData.append(size)
         return (newData)
 
+    #find a buy match for a submitted sell
     def findMatchForSell(self, data):
         newData = []
-        size = int(data[2])
-        price = int(data[3])
+        if (len(data) == 4):
+            size = int(data[2])
+            price = int(data[3])
+        else:
+            size = int(data[1])
+            price = int(data[2])
+        deleted = 0
+        #iterate priority queue and identify for any matches 
         if (len(self.buy) > 0):
             for index in range(len(self.buy)):
-                if (int(self.buy[index][2]) >= price):
-                    if (int(self.buy[index][1]) > size):
-                        self.buy[index][1] = int(self.buy[index][1]) - size
-                        size = size - int(self.buy[index][1])
-                        print("Matched with buy order number", self.buy[index][0])
-                    elif (self.buy[index][1] == size):
-                        size = size - int(self.buy[index][1])
-                        print("Matched with buy order number", self.buy[index][0])
-                        self.delete(self.buy[index][0])
+                newIndex = index - deleted
+                if (int(self.buy[newIndex][2]) >= price):
+                    if (int(self.buy[newIndex][1]) > size):
+                        print("less", size, self.buy[newIndex][1])
+                        self.buy[newIndex][1] = int(self.buy[newIndex][1]) - size
+                        size = 0
+                        print("Matched with buy order number", self.buy[newIndex][0])
+                    elif (self.buy[newIndex][1] == size):
+                        size = size - int(self.buy[newIndex][1])
+                        print("Matched with buy order number", self.buy[newIndex][0])
+                        self.delete(self.buy[newIndex][0])
+                        deleted += 1
                     else:
-                       size = size - int(self.buy[index][1])
-                       print("Matched with buy order number", self.buy[index][0])
-                       self.delete(self.buy[index][0])
+                       size = size - int(self.buy[newIndex][1])
+                       print("Matched with buy order number", self.buy[newIndex][0])
+                       self.delete(self.buy[newIndex][0])
+                       deleted += 1
+                #if all of a new order has been sold
                 if (size == 0):
                     newData.append(1)
                     newData.append(size)
                     return (newData)
+        #if some of a new order has been sold
         newData.append(0)
         newData.append(size)
         return (newData)
@@ -83,10 +112,12 @@ class OrderBook(object):
     #for finding if list is a buy or sell item and its position in that list
     def findItemInList(self, orderNumber):
         itemDetails = []
+        #check if order is in buy queue
         for index in range(len(self.buy)):
             if (int(self.buy[index][0]) == int(orderNumber)):
                 itemDetails.append("buy")
                 itemDetails.append(index)
+        #check if order is in sell queue
         if (itemDetails == []):
             for index in range(len(self.sell)):
                 if (int(self.sell[index][0]) == int(orderNumber)):
@@ -94,21 +125,40 @@ class OrderBook(object):
                     itemDetails.append(index)
         return itemDetails
 
+    #inserts buy order in its rightful place considering the priority rules
     def findRightBuyIndex(self, price):
         if (len(myOrderBook.buy) == 0):
             return 0
         else:
             for index in range(len(self.buy)):
-                if (int(self.buy[index][2]) <= int(price)):
+                if (int(self.buy[index][2]) < int(price)):
                     return index
+                elif (int(self.buy[index][2]) == int(price)):
+                    while (index < len(self.buy)):
+                        if (int(self.buy[index][2]) == int(price)):
+                            index += 1
+                        else:
+                             return index + 1
+            return (len(self.buy))
 
+    #inserts sell order in its rightful place considering the priority rules
     def findRightSellIndex(self, price):
+        print("sell:", self.sell)
         if (len(myOrderBook.sell) == 0):
-            return len(myOrderBook.sell) - 1
+            return 0
         else:
             for index in range(len(self.sell)-1, -1, -1):
-                if (int(self.sell[index][2]) <= int(price)):
+                if (int(self.sell[index][2]) == int(price)):
                     return index + 1
+                elif (int(self.sell[index][2]) < int(price)):
+                    print("here in elif")
+                    while (index >= 0):
+                        if (int(self.sell[index][2]) < int(price)):
+                            index -= 1
+                        else:
+                            return index + 1
+                    return index
+            return (0)
  
     # for inserting an element in the queue
     def insert(self, data, buyOrSell):
@@ -116,19 +166,20 @@ class OrderBook(object):
         if (buyOrSell == "B"):
             matchedData = myOrderBook.findMatchForBuy(data)
             if (int(matchedData[0]) == 0):
-                print(matchedData)
                 dataForInput = self.makeDataForInput(-1, matchedData[1], int(data[3]))
                 #enter in queue with price and time priority
                 rightIndex = myOrderBook.findRightBuyIndex(data[3])
                 self.buy.insert(rightIndex, dataForInput)
+                print("New order id: ", dataForInput[0])
         elif (buyOrSell == "S"):
             matchedData = myOrderBook.findMatchForSell(data)
             if (int(matchedData[0]) == 0):
-                print(matchedData)
                 dataForInput = self.makeDataForInput(-1, matchedData[1], int(data[3]))
                 #enter with price and time Priority
                 rightIndex = myOrderBook.findRightSellIndex(data[3])
+                print("rightIndex", rightIndex)
                 self.sell.insert(rightIndex, dataForInput)
+                print("New order id: ", dataForInput[0])
 
     # for modifying queue
     def modify(self, orderNumber, newData):
@@ -140,8 +191,14 @@ class OrderBook(object):
         orderNumber = int(orderNumber)
         myOrderBook.delete(orderNumber)        
         list_ = getattr(self, item[0])
-        list_.append(newData)
-
+        if (item[0] == "buy"):
+            results = self.findMatchForBuy(newData)
+        else:
+            results = self.findMatchForSell(newData)
+        if (results[0] == 0):
+            newData[1] = results[1]
+            list_.append(newData)
+        print("New order id: ", newData[0])
  
     # for popping an element based on Priority
     def delete(self, orderNumber):
@@ -149,7 +206,6 @@ class OrderBook(object):
         index = -1
         item = None
         orderDetails = myOrderBook.findItemInList(orderNumber)
-        print("orderdetails", orderDetails)
         try:
             if (orderDetails == []):
                 raise IndexError
@@ -178,7 +234,7 @@ if __name__ == '__main__':
             myOrderBook.modify(elements[1], myOrderBook.makeDataForInput(elements[1], elements[2], elements[3]))
         # check if it is a valid delete request
         elif (re.search("[D|d]\ +\d*\ *$", user_input)):
-            print(myOrderBook.delete(user_input.split()[1]))
+            myOrderBook.delete(user_input.split()[1])
         elif (user_input == "quit"):
             print ("Goodbye!")
             exit (0)
